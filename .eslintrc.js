c84f4437413cd8e5913d54fb7f510c9d6230e7d2
Warren Galyen <wgalyen@bellhelmets.com>
2021-04-01 14:55:06 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    root: true,
+    parserOptions: {
+      parser: 'babel-eslint',
+      ecmaVersion: 8,
+    },
+    env: {
+      es6: true,
+      node: true,
+      browser: true,
+    },
+    plugins: ['prettier'],
+    extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings', 'prettier/standard'],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          bracketSpacing: false,
+          semi: false,
+          printWidth: 120,
+        },
+      ],
+      'no-empty': [
+        'error',
+        {
+          allowEmptyCatch: true,
+        },
+      ],
+      'no-console': 0,
+    },
+    globals: {
+      io: true,
+    },
+  }
